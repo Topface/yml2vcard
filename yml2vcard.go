1f6f3913255fc1a7903ac686ac8a38b0018c6e33
Renat Akhmedyanov <renat@topface.com>
2015-09-02 18:59:12 +0300

    gofmt -w

--- yml2vcard.go
+++ yml2vcard.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"code.google.com/p/go-uuid/uuid"
 	"flag"
 	"fmt"
-	"log"
-	"time"
-	"bytes"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-    "strings"
-    "code.google.com/p/go-uuid/uuid"
+	"log"
+	"strings"
+	"time"
 )
 
 type Employee struct {
-    Id           int      `yaml:"id"`
+	Id           int      `yaml:"id"`
 	Name         string   `yaml:"name"`
 	Phone        string   `yaml:"phone"`
 	ExtraPhones  []string `yaml:"extra_phones"`
@@ -24,15 +24,15 @@ type Employee struct {
 
 func main() {
 	employeesfile := flag.String("file", "", "path to employees.yaml")
-    org := flag.String("org", "", "optional organization name")
-    suffix := flag.String("suffix", "", "optional suffix will be added to names")
-    prefix := flag.String("prefix", "", "optional prefix will be added to names")
+	org := flag.String("org", "", "optional organization name")
+	suffix := flag.String("suffix", "", "optional suffix will be added to names")
+	prefix := flag.String("prefix", "", "optional prefix will be added to names")
 	flag.Parse()
 
-    if *employeesfile == "" {
-        flag.PrintDefaults()
-        return
-    }
+	if *employeesfile == "" {
+		flag.PrintDefaults()
+		return
+	}
 
 	emps, err := parseEmployeesYml(*employeesfile)
 	if err != nil {
@@ -62,81 +62,81 @@ func parseEmployeesYml(file string) ([]Employee, error) {
 func vcardify(emp Employee, org, prefix, suffix string) string {
 	var buf bytes.Buffer
 
-    buf.WriteString("BEGIN:VCARD\n")
+	buf.WriteString("BEGIN:VCARD\n")
 	buf.WriteString("VERSION:3.0\n")
-    buf.WriteString(fmt.Sprintf(
-        "REV:%s\n", time.Now().UTC().Format("2006-01-02T15:04:05Z")))
+	buf.WriteString(fmt.Sprintf(
+		"REV:%s\n", time.Now().UTC().Format("2006-01-02T15:04:05Z")))
 
-    buf.WriteString("PRODID:-//Topface//yml2vcard//RU\n")
+	buf.WriteString("PRODID:-//Topface//yml2vcard//RU\n")
 
-    names := strings.Split(emp.Name, " ")
-    var firstname, lastname string
-    switch len(names) {
-    case 1:
-        firstname, lastname = names[0], ""
-    case 2:
-        firstname, lastname = names[0], names[1]
-    case 3:
-        firstname, lastname = names[0], names[2]
-    default:
-        firstname, lastname = emp.Name, ""
-    }
+	names := strings.Split(emp.Name, " ")
+	var firstname, lastname string
+	switch len(names) {
+	case 1:
+		firstname, lastname = names[0], ""
+	case 2:
+		firstname, lastname = names[0], names[1]
+	case 3:
+		firstname, lastname = names[0], names[2]
+	default:
+		firstname, lastname = emp.Name, ""
+	}
 
-    buf.WriteString(fmt.Sprintf(
-        "N:%s;%s;;%s;%s\n", lastname, firstname, prefix, suffix))
+	buf.WriteString(fmt.Sprintf(
+		"N:%s;%s;;%s;%s\n", lastname, firstname, prefix, suffix))
 
-    buf.WriteString("FN:")
-    if prefix != "" {
-        buf.WriteString(prefix + " ")
-    }
-    buf.WriteString(emp.Name)
-    if suffix != "" {
-        buf.WriteString(" " + suffix)
-    }
-    buf.WriteString("\n")
+	buf.WriteString("FN:")
+	if prefix != "" {
+		buf.WriteString(prefix + " ")
+	}
+	buf.WriteString(emp.Name)
+	if suffix != "" {
+		buf.WriteString(" " + suffix)
+	}
+	buf.WriteString("\n")
 
-    if org != "" {
-        buf.WriteString("ORG:")
-        buf.WriteString(org)
-        buf.WriteString("\n")
-    }
+	if org != "" {
+		buf.WriteString("ORG:")
+		buf.WriteString(org)
+		buf.WriteString("\n")
+	}
 
-    if emp.Phone != "" {
-        buf.WriteString(fmt.Sprintf(
-            "TEL:%s\n", emp.Phone))
-    }
+	if emp.Phone != "" {
+		buf.WriteString(fmt.Sprintf(
+			"TEL:%s\n", emp.Phone))
+	}
 
-    if len(emp.ExtraPhones) > 0 {
-        for _, p := range emp.ExtraPhones {
-            buf.WriteString(fmt.Sprintf(
-                "TEL:%s\n", p))
-        }
-    }
+	if len(emp.ExtraPhones) > 0 {
+		for _, p := range emp.ExtraPhones {
+			buf.WriteString(fmt.Sprintf(
+				"TEL:%s\n", p))
+		}
+	}
 
-    if emp.WorkMail != "" {
-        buf.WriteString(fmt.Sprintf(
-            "EMAIL;TYPE=work:%s\n", emp.WorkMail))
-    }
+	if emp.WorkMail != "" {
+		buf.WriteString(fmt.Sprintf(
+			"EMAIL;TYPE=work:%s\n", emp.WorkMail))
+	}
 
-    if emp.PersonalMail != "" {
-        buf.WriteString(fmt.Sprintf(
-            "EMAIL;TYPE=home:%s\n", emp.PersonalMail))
-    }
+	if emp.PersonalMail != "" {
+		buf.WriteString(fmt.Sprintf(
+			"EMAIL;TYPE=home:%s\n", emp.PersonalMail))
+	}
 
-    if len(emp.ExtraMails) > 0 {
-        for _, m := range emp.ExtraMails {
-            buf.WriteString(fmt.Sprintf(
-                "EMAIL:%s\n", m))
-        }
-    }
+	if len(emp.ExtraMails) > 0 {
+		for _, m := range emp.ExtraMails {
+			buf.WriteString(fmt.Sprintf(
+				"EMAIL:%s\n", m))
+		}
+	}
 
-    uuid := uuid.NewMD5(uuid.NIL, []byte(emp.Name))
-    buf.WriteString(fmt.Sprintf(
-        "X-RADICALE-NAME:%s.vcf\n", uuid.String()))
-    buf.WriteString(fmt.Sprintf(
-        "UID:%s\n", uuid.String()))
+	uuid := uuid.NewMD5(uuid.NIL, []byte(emp.Name))
+	buf.WriteString(fmt.Sprintf(
+		"X-RADICALE-NAME:%s.vcf\n", uuid.String()))
+	buf.WriteString(fmt.Sprintf(
+		"UID:%s\n", uuid.String()))
 
-    buf.WriteString("END:VCARD\n")
+	buf.WriteString("END:VCARD\n")
 
 	return buf.String()
 }
